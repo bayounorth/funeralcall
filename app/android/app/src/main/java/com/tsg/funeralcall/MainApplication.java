6382cc65561310ea9440670fe4dc5696d38b6b4d
BayouNorth <brian@bayounorth.com>
2025-11-18 15:34:13 -0500

    java update - app testing

--- app/android/app/src/main/java/com/tsg/funeralcall/MainApplication.java
+++ app/android/app/src/main/java/com/tsg/funeralcall/MainApplication.java
@@ -76,25 +76,6 @@ public class MainApplication extends Application implements ReactApplication {
    */
   private static void initializeFlipper(
       Context context, ReactInstanceManager reactInstanceManager) {
-    //if (BuildConfig.DEBUG) {
-//      try {
-//        /*
-//         We use reflection here to pick up the class that initializes Flipper,
-//        since Flipper library is not available in release mode
-//        */
-//        Class<?> aClass = Class.forName("com.omnienterprises.funeralcall.ReactNativeFlipper");
-//        aClass
-//            .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
-//            .invoke(null, context, reactInstanceManager);
-//      } catch (ClassNotFoundException e) {
-//        e.printStackTrace();
-//      } catch (NoSuchMethodException e) {
-//        e.printStackTrace();
-//      } catch (IllegalAccessException e) {
-//        e.printStackTrace();
-//      } catch (InvocationTargetException e) {
-//        e.printStackTrace();
-//      }
-    //}
+
   }
 }
